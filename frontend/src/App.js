908c88d214c249fc67314017ff571afef7f63ade
Eunbi <jenabill@naver.com>
2023-04-09 19:05:17 +0900

    chore(fe): add setting of react query

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,27 +2,38 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { GlobalStyle, theme } from "./styles/theme";
 import { ThemeProvider } from "styled-components";
 import { RecoilRoot } from "recoil";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Layout from "./components/layout/Layout";
 import HomePage from "./pages/HomePage";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 120000,
+    },
+  },
+});
 
 function App() {
   return (
     <RecoilRoot>
       <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <BrowserRouter>
-          <Routes>
-            <Route element={<Layout />}>
-              <Route path="/" element={<HomePage />} />
-              <Route path="recipe" element={<HomePage />} />
-              <Route path="community" element={<HomePage />} />
-              <Route path="cocktailbar" element={<HomePage />} />
-              <Route path="mypage" element={<HomePage />} />
-              <Route path="login" element={<HomePage />} />
-              <Route path="register" element={<HomePage />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <GlobalStyle />
+          <BrowserRouter>
+            <Routes>
+              <Route element={<Layout />}>
+                <Route path="/" element={<HomePage />} />
+                <Route path="recipe" element={<HomePage />} />
+                <Route path="community" element={<HomePage />} />
+                <Route path="cocktailbar" element={<HomePage />} />
+                <Route path="mypage" element={<HomePage />} />
+                <Route path="login" element={<HomePage />} />
+                <Route path="register" element={<HomePage />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </QueryClientProvider>
       </ThemeProvider>
     </RecoilRoot>
   );
