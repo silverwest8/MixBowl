6f23b4f61b229793a72566e5e6e376a287f75607
Eunbi <jenabill@naver.com>
2023-04-10 13:39:50 +0900

    feat(fe): add search bar component

---
+++ frontend/src/components/common/SearchBar.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import { FaTimesCircle, FaSearch } from "react-icons/fa";
+
+const SearchBar = ({ value, name, placholder, close, onClick, onChange }) => {
+  return (
+    <SearchBarBox>
+      <input
+        value={value}
+        name={name}
+        placeholder={placholder}
+        onChange={onChange}
+      />
+      <button onClick={onClick}>
+        {close ? <FaTimesCircle /> : <FaSearch />}
+      </button>
+    </SearchBarBox>
+  );
+};
+
+const SearchBarBox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.25rem;
+  padding: 0.7rem 1.25rem;
+  border-radius: 50px;
+  border: 2px solid ${({ theme }) => theme.color.primaryGold};
+  & > input {
+    flex-grow: 1;
+  }
+  & > input::placeholder {
+    color: ${({ theme }) => theme.color.lightGray};
+  }
+  svg {
+    font-size: 1.25rem;
+    color: ${({ theme }) => theme.color.primaryGold};
+  }
+`;
+
+export default SearchBar;
