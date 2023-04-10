96a6f699dfa2a41485dd5baf03e0821978efec4a
Eunbi <jenabill@naver.com>
2023-04-10 13:40:15 +0900

    refactor(fe): update theme object

--- frontend/src/styles/theme.js
+++ frontend/src/styles/theme.js
@@ -1,18 +1,46 @@
+import { createTheme } from "@mui/material";
 import { createGlobalStyle } from "styled-components";
 
 export const theme = {
-  black: "#111111",
-  primaryGold: "#e9aa33",
-  secondGold: "#ed9b00",
-  lightGray: "#cfcfcf",
-  darkGray: "#3e3e3e",
-  red: "#ff542e",
-  green: "#1ed900",
+  color: {
+    black: "#111111",
+    primaryGold: "#e9aa33",
+    secondGold: "#ed9b00",
+    lightGray: "#cfcfcf",
+    darkGray: "#3e3e3e",
+    red: "#ff542e",
+    green: "#1ed900",
+    gray: "#b3b3b3",
+  },
+  device: {
+    mobile: "max-width: 620px",
+  },
 };
+
+export const muiTheme = createTheme({
+  typography: {
+    fontFamily: [
+      "Pretendard",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      "system-ui",
+      "Roboto",
+      "Helvetica Neue",
+      "Segoe UI",
+      "Apple SD Gothic Neo",
+      "Noto Sans KR",
+      "Malgun Gothic",
+      "Apple Color Emoji",
+      "Segoe UI Emoji",
+      "Segoe UI Symbol",
+      "sans-serif",
+    ].join(","),
+  },
+});
 
 export const GlobalStyle = createGlobalStyle`
   html {
-    background-color: ${theme.black};
+    background-color: ${theme.color.black};
     color: white;
   }
 `;
