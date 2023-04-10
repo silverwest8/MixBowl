10e1875963c923b83521eaf0fd419c88e2a66ef9
Eunbi <jenabill@naver.com>
2023-04-10 13:40:59 +0900

    refactor(fe): add toast message and modal renderer component to App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { GlobalStyle, theme } from "./styles/theme";
+import { GlobalStyle, muiTheme, theme } from "./styles/theme";
 import { ThemeProvider } from "styled-components";
+import { ThemeProvider as MuiThemeProvider } from "@mui/material";
 import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Layout from "./components/layout/Layout";
 import HomePage from "./pages/HomePage";
+import ToastMessage from "./components/common/ToastMessage";
+import ModalRenderer from "./components/layout/ModalRenderer";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,22 +21,26 @@ function App() {
   return (
     <RecoilRoot>
       <ThemeProvider theme={theme}>
-        <QueryClientProvider client={queryClient}>
-          <GlobalStyle />
-          <BrowserRouter>
-            <Routes>
-              <Route element={<Layout />}>
-                <Route path="/" element={<HomePage />} />
-                <Route path="recipe" element={<HomePage />} />
-                <Route path="community" element={<HomePage />} />
-                <Route path="cocktailbar" element={<HomePage />} />
-                <Route path="mypage" element={<HomePage />} />
-                <Route path="login" element={<HomePage />} />
-                <Route path="register" element={<HomePage />} />
-              </Route>
-            </Routes>
-          </BrowserRouter>
-        </QueryClientProvider>
+        <MuiThemeProvider theme={muiTheme}>
+          <QueryClientProvider client={queryClient}>
+            <GlobalStyle />
+            <ToastMessage />
+            <ModalRenderer />
+            <BrowserRouter>
+              <Routes>
+                <Route element={<Layout />}>
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="recipe" element={<HomePage />} />
+                  <Route path="community" element={<HomePage />} />
+                  <Route path="cocktailbar" element={<HomePage />} />
+                  <Route path="mypage" element={<HomePage />} />
+                  <Route path="login" element={<HomePage />} />
+                  <Route path="register" element={<HomePage />} />
+                </Route>
+              </Routes>
+            </BrowserRouter>
+          </QueryClientProvider>
+        </MuiThemeProvider>
       </ThemeProvider>
     </RecoilRoot>
   );
