5036e6354e3f0e300d635c9a879f3ffd7d1ba3e9
silverwest8 <silverwest8@gmail.com>
2023-04-24 22:31:08 +0900

    🔊 Winston Setting Change

--- backend/winston/winston.js
+++ backend/winston/winston.js
@@ -1,57 +1,89 @@
 import winston from 'winston';
 import winstonDaily from 'winston-daily-rotate-file';
+import path from 'path';
 
-const logDir = 'winston/logs'; // logs 디렉토리 하위에 로그 파일 저장
-const { combine, timestamp, printf } = winston.format;
+const { combine, timestamp, printf, colorize } = winston.format;
 
-// Define log format
-const logFormat = printf(info => {
-  return `${info.timestamp} ${info.level}: ${info.message}`;
-});
+const logDir = 'logs';
 
-/*
- * Log Level
- * error: 0, warn: 1, info: 2, http: 3, verbose: 4, debug: 5, silly: 6
- */
-const logger = winston.createLogger({
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  debug: 4,
+};
+
+const colors = {
+  info: 'white',
+  error: 'red',
+  debug: 'blue',
+  warn: 'yellow',
+  http: 'magenta',
+};
+winston.addColors(colors);
+
+const level = () => {
+  const env = process.env.NODE_ENV || 'development';
+  const isDevelopment = env === 'development';
+  return isDevelopment ? 'debug' : 'http';
+};
+
+const timezoned = () => {
+  return new Date().toLocaleString('ko-KR', {
+    timeZone: 'Asia/Seoul',
+  });
+};
+
+// Log Format
+const logFormat = combine(
+  timestamp({ format: timezoned }),
+  printf(info => {
+    if (info.stack) {
+      return `${info.timestamp} ${info.level}: ${info.message} \n Error Stack: ${info.stack}`;
+    }
+    return `${info.timestamp} ${info.level}: ${info.message}`;
+  })
+);
+
+// 콘솔에 찍힐 때는 색깔을 구변해서 로깅해주자.
+const consoleOpts = {
+  handleExceptions: true,
+  level: process.env.NODE_ENV === 'production' ? 'error' : 'debug',
   format: combine(
-    timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss',
-    }),
-    logFormat
+    colorize({ all: true }),
+    timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' })
   ),
-  transports: [
-    // info 레벨 로그를 저장할 파일 설정
-    new winstonDaily({
-      level: 'info',
-      datePattern: 'YYYY-MM-DD',
-      dirname: logDir,
-      filename: `%DATE%.log`,
-      maxFiles: 30, // 30일치 로그 파일 저장
-      zippedArchive: true,
-    }),
-    // error 레벨 로그를 저장할 파일 설정
-    new winstonDaily({
-      level: 'error',
-      datePattern: 'YYYY-MM-DD',
-      dirname: logDir + '/error', // error.log 파일은 /logs/error 하위에 저장
-      filename: `%DATE%.error.log`,
-      maxFiles: 30,
-      zippedArchive: true,
-    }),
-  ],
-});
+};
 
-// Production 환경이 아닌 경우(dev 등)
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(
-    new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.colorize(), // 색깔 넣어서 출력
-        winston.format.simple() // `${info.level}: ${info.message} JSON.stringify({ ...rest })` 포맷으로 출력
-      ),
-    })
-  );
-}
+const transports = [
+  // 콘솔로그찍을 때만 색넣자.
+  new winston.transports.Console(consoleOpts),
+  // error 레벨 로그를 저장할 파일 설정
+  new winstonDaily({
+    level: 'error',
+    datePattern: 'YYYY-MM-DD',
+    dirname: path.join(__dirname, logDir, '/error'),
+    filename: '%DATE%.error.log',
+    maxFiles: 30,
+    zippedArchive: true,
+  }),
+  // 모든 레벨 로그를 저장할 파일 설정
+  new winstonDaily({
+    level: 'debug',
+    datePattern: 'YYYY-MM-DD',
+    dirname: path.join(__dirname, logDir, '/all'),
+    filename: '%DATE%.all.log',
+    maxFiles: 7,
+    zippedArchive: true,
+  }),
+];
+
+const logger = winston.createLogger({
+  level: level(),
+  levels,
+  format: logFormat,
+  transports,
+});
 
 export { logger };
