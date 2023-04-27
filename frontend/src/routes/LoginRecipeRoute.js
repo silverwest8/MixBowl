6863cbf753d467d1e41267f76395d370323028da
Eunbi <jenabill@naver.com>
2023-04-28 01:12:18 +0900

    refactor(fe): change recipe route

--- frontend/src/routes/LoginRecipeRoute.js
+++
@@ -1,7 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-const LoginRecipeRoute = () => {
-  const token = localStorage.getItem("access_token");
-  return token ? <Outlet /> : <Navigate to={"notloginrecipe"} replace />;
-};
-
-export default LoginRecipeRoute;
