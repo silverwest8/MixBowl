dfb7d95ca864a3e5dba04768a871dbb96f00fc59
Eunbi <jenabill@naver.com>
2023-04-27 17:31:03 +0900

    fix(fe): fix router error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { GlobalStyle, muiTheme, theme } from "./styles/theme";
 import { ThemeProvider } from "styled-components";
 import { ThemeProvider as MuiThemeProvider } from "@mui/material";
-import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useStaySignedIn } from "./hooks/useStaySignedIn";
 import Layout from "./components/layout/Layout";
@@ -28,30 +27,28 @@ const queryClient = new QueryClient({
 function App() {
   useStaySignedIn();
   return (
-    <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <MuiThemeProvider theme={muiTheme}>
-          <QueryClientProvider client={queryClient}>
-            <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <MuiThemeProvider theme={muiTheme}>
+        <QueryClientProvider client={queryClient}>
+          <GlobalStyle />
+          <BrowserRouter>
             <ToastMessage />
             <ModalRenderer />
-            <BrowserRouter>
-              <Routes>
-                <Route element={<Layout />}>
-                  <Route path="/" element={<HomePage />} />
-                  <Route path="recipe" element={<HomePage />} />
-                  <Route path="community" element={<HomePage />} />
-                  <Route path="cocktailbar" element={<HomePage />} />
-                  <Route path="mypage" element={<HomePage />} />
-                  <Route path="login" element={<LoginPage />} />
-                  <Route path="register" element={<RegisterPage />} />
-                </Route>
-              </Routes>
-            </BrowserRouter>
-          </QueryClientProvider>
-        </MuiThemeProvider>
-      </ThemeProvider>
-    </RecoilRoot>
+            <Routes>
+              <Route element={<Layout />}>
+                <Route path="/" element={<HomePage />} />
+                <Route path="recipe" element={<HomePage />} />
+                <Route path="community" element={<HomePage />} />
+                <Route path="cocktailbar" element={<HomePage />} />
+                <Route path="mypage" element={<HomePage />} />
+                <Route path="login" element={<LoginPage />} />
+                <Route path="register" element={<RegisterPage />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </QueryClientProvider>
+      </MuiThemeProvider>
+    </ThemeProvider>
   );
 }
 
