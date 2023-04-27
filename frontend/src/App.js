3426b9211e3691b9bc709a3eaab85e0d084b18c0
Eunbi <jenabill@naver.com>
2023-04-28 00:49:37 +0900

    fix conflict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import { GlobalStyle, muiTheme, theme } from "./styles/theme";
 import { ThemeProvider } from "styled-components";
 import { ThemeProvider as MuiThemeProvider } from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-<<<<<<< HEAD
 import { useStaySignedIn } from "./hooks/useStaySignedIn";
 import Layout from "./components/layout/Layout";
 import HomePage from "./pages/HomePage";
@@ -17,17 +16,9 @@ import LoginRecipeRoute from "./routes/LoginRecipeRoute";
 import NotLoginRecipeRoute from "./routes/NotLoginRecipeRoute.js";
 import ToastMessage from "./components/common/ToastMessage";
 import ModalRenderer from "./components/layout/ModalRenderer";
-=======
-import Layout from "./components/layout/Layout";
-import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/LoginPage";
 import CocktailBarPage from "./pages/CocktailBarPage";
 import CommunityHomePage from "./pages/CommunityHomePage";
 import PostingPage from "./pages/PostingPage";
-import ToastMessage from "./components/common/ToastMessage";
-import ModalRenderer from "./components/layout/ModalRenderer";
-import { useStaySignedIn } from "./hooks/useStaySignedIn";
->>>>>>> 62abeae19c34145babe6aa694166c721e22cba23
 
 import "swiper/css";
 import "swiper/css/navigation";
@@ -55,7 +46,6 @@ function App() {
             <Routes>
               <Route element={<Layout />}>
                 <Route path="/" element={<HomePage />} />
-<<<<<<< HEAD
                 <Route element={<LoginRecipeRoute />}>
                   <Route path="recipe" element={<RecipePage />} />
                   <Route path="/recipe/:id" element={<DetailRecipePage />} />
@@ -67,22 +57,14 @@ function App() {
                     element={<NotLoginRecipePage />}
                   />
                 </Route>
-                <Route path="community" element={<HomePage />} />
-                <Route path="cocktailbar" element={<HomePage />} />
                 <Route path="mypage" element={<HomePage />} />
                 <Route path="login" element={<LoginPage />} />
                 <Route path="register" element={<RegisterPage />} />
-=======
-                <Route path="recipe" element={<HomePage />} />
                 <Route path="cocktailbar" element={<CocktailBarPage />}>
                   <Route path=":id" element={<CocktailBarPage />} />
                 </Route>
                 <Route path="community" element={<CommunityHomePage />} />
                 <Route path="community/posting" element={<PostingPage />} />
-                <Route path="mypage" element={<HomePage />} />
-                <Route path="login" element={<LoginPage />} />
-                <Route path="register" element={<HomePage />} />
->>>>>>> 62abeae19c34145babe6aa694166c721e22cba23
               </Route>
             </Routes>
           </BrowserRouter>
