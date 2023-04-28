07153c2eb72f9fbd310887a1236be5b3781db7ce
Eunbi <jenabill@naver.com>
2023-04-28 13:37:37 +0900

    refactor(fe): add cocktailbar api functions

---
+++ frontend/src/api/cocktailbar.js
@@ -0,0 +1,30 @@
+import { getAccessToken } from "../utils/token";
+import axios from "axios";
+
+export const getCocktailBar = async ({ queryKey }) => {
+  const token = getAccessToken();
+  axios.defaults.headers.common.Authorization = token;
+  const { data } = await axios.get(`/api/review/getBar/${queryKey[1]}`);
+  return data;
+};
+
+export const getReview = async ({ queryKey }) => {
+  const token = getAccessToken();
+  axios.defaults.headers.common.Authorization = token;
+  const { data } = await axios.get(`/api/review/getReview/${queryKey[1]}`);
+  return data;
+};
+
+export const getCocktailBarList = async ({ queryKey }) => {
+  const accessToken = getAccessToken();
+  if (accessToken)
+    axios.defaults.headers.common.Authorization = getAccessToken();
+  if (queryKey[1] !== null) {
+    const { data } = await axios.get(
+      `/api/review/getList?query=${queryKey[2] || "칵테일 바"}&x=${
+        queryKey[1].lng
+      }&y=${queryKey[1].lat}&radius=${queryKey[3]}&sort=accuracy`
+    );
+    return data;
+  } else return null;
+};
