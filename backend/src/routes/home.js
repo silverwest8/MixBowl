31b64e1c419e4fcb60ffb577aa23eb49a519c5eb
silverwest8 <silverwest8@gmail.com>
2023-05-24 01:14:59 +0900

    feat(be): add home apis

--- backend/src/routes/home.js
+++ backend/src/routes/home.js
@@ -2,11 +2,45 @@
 
 import express from 'express';
 import { db } from '../models';
+import { Sequelize } from 'sequelize';
 const router = express.Router();
 
-router.get('/questions', async (req, res) => {
+router.get('/question', async (req, res) => {
   try {
     let list = [];
+    const questions = await db.POST.findAll({
+      where: {
+        category: 2,
+      },
+      include: [
+        {
+          model: db.POST_REPLY,
+          as: 'POST_REPLies',
+          required: false,
+        },
+        {
+          model: db.USER,
+          as: 'UNO_USER',
+          attributes: ['NICKNAME', 'LEVEL'],
+          required: false,
+        },
+      ],
+    });
+    for (let i = 0; i < questions.length; i++) {
+      const question = questions[i];
+      if (question.POST_REPLies.length == 0) {
+        let temp = {
+          postId: question.PNO,
+          content: question.CONTENT,
+          date: question.createdAt,
+          USER: {
+            nickname: question.UNO_USER.NICKNAME,
+            level: question.UNO_USER.LEVEL,
+          },
+        };
+        list.push(temp);
+      }
+    }
     return res.status(200).json({
       success: true,
       message: 'Home question get 성공',
@@ -24,6 +58,66 @@ router.get('/questions', async (req, res) => {
 router.get('/cocktail', async (req, res) => {
   try {
     let list = [];
+    // 기준 불확실
+    const cocktails = await db.COCKTAIL.findAll({
+      where: {}, // 일주일 필터링 넣으면 아무것도 안보여서 보류
+      attributes: {
+        include: [
+          [Sequelize.fn('COUNT', Sequelize.col('COCKTAIL_LIKEs.UNO')), 'COUNT'],
+        ],
+      },
+      include: [
+        {
+          model: db.COCKTAIL_LIKE,
+          as: 'COCKTAIL_LIKEs',
+          attributes: [],
+        },
+        {
+          model: db.USER,
+          as: 'UNO_USER',
+          required: false,
+          attributes: ['NICKNAME'],
+        },
+      ],
+      having: {
+        COUNT: { [Sequelize.Op.gt]: 0 },
+      },
+      group: ['COCKTAIL.CNO'],
+      order: [
+        [Sequelize.literal('COUNT'), 'DESC'],
+        ['createdAt', 'DESC'],
+      ],
+    });
+    for (let i = 0; i < cocktails.length; i++) {
+      const cocktail = cocktails[i];
+      const content = await db.POST.findOne({
+        where: {
+          CATEGORY: 3,
+          CNO: cocktail.CNO,
+        },
+        order: [['createdAt', 'DESC']],
+      });
+      let temp = {
+        cocktailId: cocktail.CNO,
+        cocktailName: cocktail.NAME,
+        reviewContent: content ? content.CONTENT : null,
+        like: await db.COCKTAIL_LIKE.count({
+          where: {
+            CNO: cocktail.CNO,
+          },
+        }),
+        reply: await db.POST.count({
+          where: {
+            CATEGORY: 3,
+            CNO: cocktail.CNO,
+          },
+        }),
+        USER: {
+          nickname: cocktail.UNO_USER.NICKNAME,
+        },
+      };
+      list.push(temp);
+    }
     return res.status(200).json({
       success: true,
       message: 'Home cocktail get 성공',
@@ -58,6 +152,60 @@ router.get('/bar', async (req, res) => {
 router.get('/community', async (req, res) => {
   try {
     let list = [];
+    const posts = await db.POST.findAll({
+      where: {}, // 일주일 필터링 넣으면 아무것도 안보여서 보류
+      attributes: {
+        include: [
+          [Sequelize.fn('COUNT', Sequelize.col('POST_LIKEs.UNO')), 'COUNT'],
+        ],
+      },
+      include: [
+        {
+          model: db.POST_LIKE,
+          as: 'POST_LIKEs',
+          attributes: [],
+        },
+        {
+          model: db.USER,
+          as: 'UNO_USER',
+          required: false,
+          attributes: ['NICKNAME', 'LEVEL'],
+        },
+      ],
+      having: {
+        COUNT: { [Sequelize.Op.gt]: 0 },
+      },
+      group: ['POST.PNO'],
+      order: [
+        [Sequelize.literal('COUNT'), 'DESC'],
+        ['createdAt', 'DESC'],
+      ],
+    });
+    for (let i = 0; i < posts.length; i++) {
+      const post = posts[i];
+      let temp = {
+        postId: post.PNO,
+        category: post.CATEGORY,
+        title: post.TITLE,
+        content: post.CONTENT,
+        like: await db.POST_LIKE.count({
+          where: {
+            PNO: post.PNO,
+          },
+        }),
+        reply: await db.POST_REPLY.count({
+          where: {
+            PNO: post.PNO,
+          },
+        }),
+        date: post.createdAt,
+        USER: {
+          nickname: post.UNO_USER.NICKNAME,
+          level: post.UNO_USER.LEVEL,
+        },
+      };
+      list.push(temp);
+    }
     return res.status(200).json({
       success: true,
       message: 'Home community get 성공',
--- backend/src/routes/review.js
+++ backend/src/routes/review.js
@@ -5,10 +5,7 @@ import multer from 'multer';
 import fs from 'fs';
 import checkAccess from '../middleware/checkAccessToken';
 import axios from 'axios';
-import PLACE from '../models/PLACE';
-import REVIEW from '../models/REVIEW';
-import USER from '../models/USER';
-import KEYWORD from '../models/KEYWORD';
+import { db } from '../models';
 import { Sequelize } from 'sequelize';
 import { logger } from '../../winston/winston';
 import sql from '../database/sql';
@@ -54,14 +51,14 @@ const KEYWORD_VALUE = [
 
 async function getKeyword(placeId) {
   let keywordlist = [null, null, null];
-  const keyword = await KEYWORD.findAll({
+  const keyword = await db.KEYWORD.findAll({
     attributes: [
       'KEYWORD',
-      [Sequelize.fn('count', Sequelize.col('*')), 'COUNT'],
+      [Sequelize.fn('COUNT', Sequelize.col('*')), 'COUNT'],
     ],
     include: [
       {
-        model: REVIEW,
+        model: db.REVIEW,
         as: 'REVIEW',
         attributes: [],
         where: { PLACE_ID: placeId },
@@ -80,7 +77,7 @@ async function getKeyword(placeId) {
 
 async function getKeywordByReviewId(reviewId) {
   const keywordlist = [];
-  const keyword = await KEYWORD.findAll({
+  const keyword = await db.KEYWORD.findAll({
     where: {
       REVIEW_ID: reviewId,
     },
@@ -132,7 +129,7 @@ router.get('/barlist', checkAccess, async (req, res) => {
         };
         const element = response.data.documents[i];
         if (element.category_name == '음식점 > 술집 > 칵테일바') {
-          const [place, created] = await PLACE.findOrCreate({
+          const [place, created] = await db.PLACE.findOrCreate({
             where: { PLACE_ID: element.id },
             defaults: {
               NAME: element.place_name,
@@ -157,14 +154,14 @@ router.get('/barlist', checkAccess, async (req, res) => {
           }
 
           data.total_cnt++;
-          const review_num = await REVIEW.findAll({
+          const review_num = await db.REVIEW.findAll({
             where: { PLACE_ID: element.id },
           });
-          const reviewList = await REVIEW.findAll({
+          const reviewList = await db.REVIEW.findAll({
             where: { PLACE_ID: element.id },
             include: [
               {
-                model: USER,
+                model: db.USER,
                 as: 'UNO_USER',
                 attributes: ['UNO', 'NICKNAME', 'LEVEL'],
                 required: false,
@@ -173,7 +170,7 @@ router.get('/barlist', checkAccess, async (req, res) => {
             order: [['createdAt', 'DESC']],
             limit: 2,
           });
-          const rating = await REVIEW.findOne({
+          const rating = await db.REVIEW.findOne({
             attributes: [
               'PLACE_ID',
               [Sequelize.fn('AVG', Sequelize.col('RATING')), 'AVG_RATING'],
@@ -231,10 +228,10 @@ router.get('/bar/:placeId', checkAccess, async (req, res) => {
   let data = {};
   const placeId = req.params.placeId;
   console.log(placeId);
-  const place_data = await PLACE.findByPk(placeId);
+  const place_data = await db.PLACE.findByPk(placeId);
   data = Object.assign(place_data.dataValues);
   try {
-    const rating = await REVIEW.findOne({
+    const rating = await db.REVIEW.findOne({
       attributes: [
         'PLACE_ID',
         [Sequelize.fn('AVG', Sequelize.col('RATING')), 'AVG_RATING'],
@@ -267,11 +264,11 @@ router.get('/bar/reviewlist/:place_id', checkAccess, async (req, res) => {
       list: [],
     };
     const place_id = req.params.place_id;
-    const review = await REVIEW.findAll({
+    const review = await db.REVIEW.findAll({
       where: { PLACE_ID: place_id },
       include: [
         {
-          model: USER,
+          model: db.USER,
           as: 'UNO_USER',
           attributes: ['UNO', 'NICKNAME', 'LEVEL'],
           required: false,
