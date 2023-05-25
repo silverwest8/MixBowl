4d0d6fee52dff63e036ceee2ab5306cc0d80d763
Eunbi <jenabill@naver.com>
2023-05-25 12:57:17 +0900

    refactor(fe): update navbar component

--- admin/src/components/layout/NavBar.js
+++ admin/src/components/layout/NavBar.js
@@ -1,15 +1,7 @@
 import { Button } from "@tremor/react";
 import { deleteCookie } from "cookies-next";
-import { usePathname } from "next/navigation";
-
-const navigation = [{ name: "콘텐츠 관리", href: "/" }];
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
 
 export default function NavBar() {
-  const pathname = usePathname();
   function logout() {
     deleteCookie("token");
     window.location.reload();
@@ -19,23 +11,8 @@ export default function NavBar() {
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 justify-between">
           <div className="flex">
-            <div className="flex flex-shrink-0 items-center">Cocktell</div>
-            <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
-              {navigation.map((item) => (
-                <a
-                  key={item.name}
-                  href={item.href}
-                  className={classNames(
-                    pathname === item.href
-                      ? "border-slate-500 text-gray-900"
-                      : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300",
-                    "inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-                  )}
-                  aria-current={pathname === item.href ? "page" : undefined}
-                >
-                  {item.name}
-                </a>
-              ))}
+            <div className="flex flex-shrink-0 items-center font-bold text-xl">
+              Cocktell 관리자 페이지
             </div>
           </div>
           <div className="flex items-center">
