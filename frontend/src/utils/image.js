29e44a8070bdf4ecbde9b98c134bfde2135db37f
Eunbi <jenabill@naver.com>
2023-05-30 16:08:21 +0900

    fix(fe): update image url

--- frontend/src/utils/image.js
+++ frontend/src/utils/image.js
@@ -11,9 +11,9 @@ export const getReviewImageUrl = (imageId) => {
 };
 
 export const getCommunityImageUrl = (imageId) => {
-  return `${process.env.REACT_APP_BACKEND_BASE_URL}/communities/one/image?imageId=${imageId}`;
+  return `/api/communities/one/image?imageId=${imageId}`;
 };
 
 export const getCocktailImageUrl = (cocktailId) => {
-  return `${process.env.REACT_APP_BACKEND_BASE_URL}/recipes/image/${cocktailId}`;
+  return `/api/recipes/image/${cocktailId}`;
 };
